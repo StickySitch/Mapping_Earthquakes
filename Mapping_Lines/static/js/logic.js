8f1781d88f107314dd053836a13229f6b68cc51f
Jack Jones <fuzzydunlop19@gmail.com>
2022-01-19 00:05:39 -0800

    Uploading complete map with line routing example.

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,27 +2,25 @@
 console.log('working!');
 
 // Creating a map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// getting data from cities
-let cityData = cities;
+let line = [
+  [37.615223, -122.389977],
+  [30.193489, -97.665010],
+  [43.6777, -79.6248],
+  [40.641766, -73.780968]
+];
 
-// Looping through City Array
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        color: 'orange',
-        weight: 4
-    })
-    .bindPopup('<h2>' + city.city + ", " + city.state + '</h2> <hr> <h3>Population ' + city.population.toLocaleString() + '</h3>')
-    .addTo(map)
-});
-
-
+L.polyline(line, {
+    color: "blue",
+    weight: 4,
+    dashArray: "5, 10",
+    dashOffset: "0"
+    
+}).addTo(map);
 // Adding tileLayer
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: apiKey
