0089f4b8f7cb4ae340636cd63491c2b586d47fa7
Jack Jones <fuzzydunlop19@gmail.com>
2022-01-23 19:34:32 -0800

    Added color legend for earthquake magnitudes

---
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -0,0 +1,151 @@
+// Testing to see if we are up and running!
+console.log('working!');
+
+// Creating a map object with a center and zoom level.
+//let map = L.map('mapid').setView([30, 30], 2);
+
+
+// Add GeoJSON data.
+let earthquakeData = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+
+
+// Adding tileLayer
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: apiKey
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: apiKey
+});
+
+//Creating a base layer that holds both map types
+let baseMaps = {
+  'Streets': streets,
+  'Satellite': satStreets
+};
+
+let earthquakes = new L.layerGroup();
+
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+let myStyle = {
+  color: '#ffffa1',
+  weight: 2
+}
+
+// Grabbing our GeoJSON data and adding it to the map.
+d3.json(earthquakeData).then(function(data) {
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into a function
+  // to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+    return 1;
+  }
+    return magnitude * 4;
+  }
+
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+  }
+
+
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+      },
+    style: styleInfo,
+      //Creating pop up menus
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup('Magnitude: ' + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(earthquakes);
+
+    earthquakes.addTo(map);
+});
+
+// Creating a legend control object.
+let legend = L.control({
+  position: 'bottomright'
+});
+
+legend.onAdd = function () {
+
+    let div = L.DomUtil.create('div', 'info legend');
+
+    const magnitudes  = [0, 1, 2, 3, 4, 5];
+  
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
+
+
